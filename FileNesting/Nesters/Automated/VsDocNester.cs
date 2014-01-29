461dc4244e07c8365887936344097e2b62983ff8
Mads Kristensen <post@madskristensen.net>
2014-01-28 21:35:25 -0800

    Added -vsdoc.js rule.

---
+++ FileNesting/Nesters/Automated/VsDocNester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using EnvDTE;
+using Microsoft.VisualStudio.Utilities;
+
+namespace MadsKristensen.FileNesting
+{
+    [Export(typeof(IFileNester))]
+    [Name("VsDoc Nester")]
+    [Order(After = "Known File Type Nester")]
+    internal class VsDocNester : IFileNester
+    {
+        public NestingResult Nest(string fileName)
+        {
+            if (!fileName.EndsWith("-vsdoc.js", StringComparison.OrdinalIgnoreCase))
+                return NestingResult.Continue;
+
+            string parent = fileName.Replace("-vsdoc.js", ".js");
+            ProjectItem item = FileNestingPackage.DTE.Solution.FindProjectItem(parent);
+
+            if (item != null)
+            {
+                item.ProjectItems.AddFromFile(fileName);
+                return NestingResult.StopProcessing;
+            }
+
+            return NestingResult.Continue;
+        }
+
+
+        public bool IsEnabled()
+        {
+            return FileNestingPackage.Options.EnableVsDocRule;
+        }
+    }
+}
