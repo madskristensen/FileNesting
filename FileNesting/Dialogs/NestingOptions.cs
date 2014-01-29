9b9f9eb3351e94c64bdcb3cfaa2b6ffd08ac59c7
Mads Kristensen <post@madskristensen.net>
2014-01-28 21:35:34 -0800

    Fixed default values in options

--- FileNesting/Dialogs/NestingOptions.cs
+++ FileNesting/Dialogs/NestingOptions.cs
@@ -5,66 +5,47 @@ namespace MadsKristensen.FileNesting
 {
     public class NestingOptions : DialogPage
     {
-        private bool _enableAutoNesting = false;
-        private bool _enableExtensionRule = true;
-        private bool _enablePathSegmentRule = true;
-        private bool _enableBundleRule = true;
-        private bool _enableSpriteRule = true;
-        private bool _enableKnownFileTypeRule = true;
-
         [LocDisplayName("Enable auto-nesting")]
         [Description("Enables automatic nesting when files are added or renamed")]
         [Category("File Nesting")]
-        public bool EnableAutoNesting
-        {
-            get { return _enableAutoNesting; }
-            set { _enableAutoNesting = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnableAutoNesting { get; set; }
 
         // Rules
         [LocDisplayName("Enable extention rule")]
         [Description("Files with an added extension nests under parent. Example: foo.js.map nests under foo.js")]
         [Category("Nesting rules")]
-        public bool EnableExtensionRule
-        {
-            get { return _enableExtensionRule; }
-            set { _enableExtensionRule = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnableExtensionRule { get; set; }
 
         [LocDisplayName("Enable path segment rule")]
         [Description("Files with an added path segment nests under parent. Example: foo.min.js nests under foo.js")]
         [Category("Nesting rules")]
-        public bool EnablePathSegmentRule
-        {
-            get { return _enablePathSegmentRule; }
-            set { _enableExtensionRule = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnablePathSegmentRule { get; set; }
 
         [LocDisplayName("Enable .bundle file rule")]
         [Description("Files with .bundle extension will be parent. Example: foo.js.bundle will nest foo.js under it")]
         [Category("Nesting rules")]
-        public bool EnableBundleRule
-        {
-            get { return _enableBundleRule; }
-            set { _enableBundleRule = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnableBundleRule { get; set; }
 
         [LocDisplayName("Enable .sprite file rule")]
         [Description("Files with .sprite extension will be parent. Example: foo.sprite will nest foo.png under it")]
         [Category("Nesting rules")]
-        public bool EnableSpriteRule
-        {
-            get { return _enableSpriteRule; }
-            set { _enableSpriteRule = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnableSpriteRule { get; set; }
 
         [LocDisplayName("Enable known file type rule")]
         [Description("Some known file types will be nestd. Example: foo.css will nested under foo.less")]
         [Category("Nesting rules")]
-        public bool EnableKnownFileTypeRule
-        {
-            get { return _enableKnownFileTypeRule; }
-            set { _enableKnownFileTypeRule = value; }
-        }
+        [DefaultValue(true)]
+        public bool EnableKnownFileTypeRule { get; set; }
+
+        [LocDisplayName("Enable -vsdoc.js rule")]
+        [Description("Nest JavaScript files ending in -vsdoc.js under corresponding .js files")]
+        [Category("Nesting rules")]
+        [DefaultValue(true)]
+        public bool EnableVsDocRule { get; set; }
     }
 }
