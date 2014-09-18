e1b689bef08c86bcedb691200446fb8e2dfe5604
Mads Kristensen <post@madskristensen.net>
2014-09-18 11:43:41 -0700

    Updated description and copyright

--- FileNesting/Properties/AssemblyInfo.cs
+++ FileNesting/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("File Nesting")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatically nest files based on file name and enables developers to nest and unnest any file manually")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mads Kristensen")]
 [assembly: AssemblyProduct("File Nesting")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright 2014 Mads Kristensen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
