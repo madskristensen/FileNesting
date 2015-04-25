602796b1e0a14750f605f8b8927488ada8f682ee
Andrew Brown <andy@andygbrown.co.uk>
2015-04-25 13:00:44 +0100

    Make nesting work in a cordova project using SetItemAttribute

--- FileNesting/Nesters/ManualNester.cs
+++ FileNesting/Nesters/ManualNester.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using EnvDTE;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
 
 namespace MadsKristensen.FileNesting
 {
     static class ManualNester
     {
+        private const string CordovaKind = "{262852C6-CD72-467D-83FE-5EEB1973A190}";
         public static void Nest(IEnumerable<ProjectItem> items)
         {
             ItemSelector selector = new ItemSelector(items);
@@ -17,10 +23,44 @@ namespace MadsKristensen.FileNesting
             {
                 string path = item.FileNames[0];
                 ProjectItem parent = item.DTE.Solution.FindProjectItem(selector.SelectedFile);
+                if (parent == null) continue;
 
-                if (parent != null)
-                    parent.ProjectItems.AddFromFile(path);
+                bool mayNeedAttributeSet = item.ContainingProject.Kind == CordovaKind;
+
+                if (mayNeedAttributeSet) DeleteAndAdd(item, path);
+                ProjectItem newItem = parent.ProjectItems.AddFromFile(path);
+                if (mayNeedAttributeSet)
+                {
+                    //  It is possible this may not be required in future cordova project versions
+                    //   so do a test first
+                    var pitems = parent.ProjectItems
+                                       .Cast<ProjectItem>();
+                    if (!pitems.Any(x => x.Name == path))
+                    {
+                        SetDependentUponAttribute(newItem, parent);
+                    }
+                }
             }
+        }
+
+        private static void SetDependentUponAttribute(ProjectItem item, ProjectItem parent)
+        {
+            //  See: https://msdn.microsoft.com/en-us/library/bb458043.aspx "How to: Add an Attribute to a Project Item"
+            Project project = item.ContainingProject;
+            
+            string uniqueName = project.UniqueName;
+            var solution = (IVsSolution)Package.GetGlobalService(typeof(IVsSolution));
+            IVsHierarchy hierarchy;
+            solution.GetProjectOfUniqueName(uniqueName, out hierarchy);
+            var buildPropertyStorage = hierarchy as IVsBuildPropertyStorage;
+            if (buildPropertyStorage != null)
+            {
+                uint itemId;
+                string fullPath = (string)item.Properties.Item("FullPath").Value;
+                hierarchy.ParseCanonicalName(fullPath, out itemId);
+                buildPropertyStorage.SetItemAttribute(itemId, "DependentUpon", parent.Name);
+            }
+            project.Save();
         }
 
         public static void UnNest(ProjectItem item)
