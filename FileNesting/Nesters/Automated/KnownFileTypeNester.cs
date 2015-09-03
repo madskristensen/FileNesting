dd325d4b6d0e76e0def593ed6c2db756093fad9e
Mads Kristensen <post@madskristensen.net>
2015-09-02 20:10:29 -0700

    Removed .bundle extension

--- FileNesting/Nesters/Automated/KnownFileTypeNester.cs
+++ FileNesting/Nesters/Automated/KnownFileTypeNester.cs
@@ -7,10 +7,9 @@ namespace MadsKristensen.FileNesting
     internal class KnownFileTypeNester : IFileNester
     {
         private static Dictionary<string, string[]> _mapping = new Dictionary<string, string[]>(){
-            {".js", new [] {".coffee", ".iced", ".ts", ".bundle", ".html", ".cshtml", ".vbhtml", ".aspx", ".master", ".ascx"}},
-            {".css", new [] {".less", ".scss", ".sass", ".bundle", ".html", ".cshtml", ".vbhtml", ".aspx", ".master", ".ascx"}},
+            {".js", new [] {".coffee", ".litcoffee", ".iced", ".ts", ".dart", ".html", ".cshtml", ".vbhtml", ".aspx", ".master", ".ascx"}},
+            {".css", new [] {".less", ".scss", ".sass", ".styl", ".html", ".cshtml", ".vbhtml", ".aspx", ".master", ".ascx"}},
             {".ts", new [] {".html", ".cshtml", ".vbhtml", ".aspx", ".master", ".ascx"}},
-            {".html", new [] {".bundle"}},
             {".map", new [] {".js", ".css"}},
         };
 
